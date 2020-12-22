19986831371825d9810da69a03d0c198469f809d
Einar Stenberg <einar@stenberg.im>
2020-12-22 09:28:50 +0100

    Create config.mk

---
+++ config.mk
@@ -0,0 +1,29 @@
+# rpi3 for Raspberry Pi 3; rpi2 for the version 2, zerow for ZeroW
+BOARD = rpi4
+
+# Hardware configuration
+PLATFORM = v2-hdmi
+
+# Target hostname
+HOSTNAME = pikvm
+
+# ru_RU, etc. UTF-8 only
+LOCALE = en_US
+
+# See /usr/share/zoneinfo
+TIMEZONE = Europe/Oslo
+
+# For SSH root user
+ROOT_PASSWD = root
+
+# Web UI credentials: user=admin, password=<this>
+WEBUI_ADMIN_PASSWD = admin
+
+# IPMI credentials: user=admin, password=<this>
+IPMI_ADMIN_PASSWD = admin
+
+# SD card device
+CARD = /dev/mmcblk0
+
+WIFI_ESSID = "AP3X"
+WIFI_PASSWD = "wehasinter.net"
